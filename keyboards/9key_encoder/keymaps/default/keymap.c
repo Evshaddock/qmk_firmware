769a2d84b2246e9e18154cceb4692aea99c0b5f2
Ben Massingill <431715+urmamasllama@users.noreply.github.com>
2018-12-24 20:16:55 -0330

    Update keymap.c
    Improved layout

--- keyboards/9key_encoder/keymaps/default/keymap.c
+++ keyboards/9key_encoder/keymaps/default/keymap.c
@@ -3,7 +3,7 @@
 // Tap Dance Declarations
 enum {
   ENT_5 = 0,
-  ZERO_7
+  ZERO_1
 };
 
 // Macro Declarations
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* LAYER 0
  * ,-----------------------.
- * |   1   |   2   |   3   |
+ * |   7   |   8   |   9   |
  * |-------+-------+-------|
  * |   4   | 5/ENT |   6   | Dbl Tap 5 for Enter
  * |-------+-------+-------|
- * |  7/0  |   8   | 9/FN  | 7/0 = Dbl Tap 7 for 0  -  9/FN = Hold 9 for FN
+ * |  1/0  |   2   | 3/FN  | 1/0 = Dbl Tap 1 for 0  -  3/FN = Hold 3 for FN
  * `---------------+-------|
  *                 | PLAY  | Clicking the Encoder - Play/Pause
  *                 |-------|
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [0] = LAYOUT( \
-  KC_1,       KC_2,      KC_3,        KC_NO,   \
-  KC_4,       TD(ENT_5), KC_6,        KC_NO,   \
-  TD(ZERO_7), KC_8,      LT(2, KC_9), KC_NO,   \
+  KC_KP_7,    KC_KP_8,   KC_KP_9,     KC_NO,   \
+  KC_KP_4,    TD(ENT_5), KC_KP_6,     KC_NO,   \
+  TD(ZERO_1), KC_KP_2,   LT(2, KC_KP_3), KC_NO,   \
   KC_NO,      KC_NO,     KC_NO,       KC_MPLY, \
   KC_NO,      KC_NO,     KC_NO,       KC_VOLU, \
   KC_NO,      KC_NO,     KC_NO,       MO(1)    \
@@ -51,18 +51,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------|
  * |  BSPC |   *   |   /   |
  * |-------+-------+-------|
- * |  00   |   .   |       |
+ * |numlock|   .   |       |
  * `-----------------------|
- *                 | NEXT  | Clicking the Encoder - Next Track 
+ *                 | NEXT  | Clicking the Encoder - Next Track
  *                 |-------|
  *                 | SCROLL| Rotating the Encoder - Scroll Wheel
  *                 `-------'
  */
 
 [2] = LAYOUT( \
-  KC_ESC,   KC_PLUS,  KC_MINS,  KC_NO,   \
-  KC_BSPC,  KC_ASTR,  KC_SLSH,  KC_NO,   \
-  M(DBL_0), KC_DOT,   KC_TRNS,  KC_NO,   \
+  KC_ESC,   KC_KP_PLUS,  KC_KP_MINS,  KC_NO,   \
+  KC_BSPC,  KC_PAST,  KC_PSLS,  KC_NO,   \
+  KC_NLCK, KC_PDOT,   KC_TRNS,  KC_NO,   \
   KC_NO,    KC_NO,    KC_NO,    KC_MNXT, \
   KC_NO,    KC_NO,    KC_NO,    KC_WH_U, \
   KC_NO,    KC_NO,    KC_NO,    MO(3)    \
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [ENT_5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_ENT),
-  [ZERO_7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_0)
+  [ENT_5] = ACTION_TAP_DANCE_DOUBLE(KC_KP_5, KC_KP_ENT),
+  [ZERO_1] = ACTION_TAP_DANCE_DOUBLE(KC_KP_1, KC_KP_0)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
